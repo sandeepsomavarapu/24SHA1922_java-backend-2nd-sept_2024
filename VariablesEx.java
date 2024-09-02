40cd9c0803927c9e413db866cc8853058e500484
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-09-02 13:46:11 +0530

    Day-1 Docs

---
+++ VariablesEx.java
@@ -0,0 +1,17 @@
+class VariablesEx
+{
+	short price=28900;//global variable
+	public static void main(String args[])
+	{
+		//local variables
+	
+		byte age=21;
+		long contact=92342384923l;
+		float salary=23.987129821732f;
+		double marks=23.987129821732;
+		char gender='M';
+		boolean result=true;	
+		
+	System.out.println(age+" "+contact+" "+salary+" "+marks+" "+gender+" "+result);//printing statement
+	}
+}
---
+++ Welcome.java
@@ -0,0 +1,15 @@
+class Welcome
+{
+	public static void main(String args[])
+	{
+	System.out.println("welcome to java");//printing statement
+	}
+}
+
+//user understandable lang -->.java --->JVM
+//JVM-->0's 1's binary code byte code
+//compailation-->compiler   --->user--->machine
+
+//source code--->byte code-->compailation-->compiler
+//javac filename.java
+//.class file  byte code -->JVM
